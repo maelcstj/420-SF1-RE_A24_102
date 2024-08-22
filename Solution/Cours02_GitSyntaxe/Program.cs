d4b3fb5b8892d63813d90cd05595f356507fe2bd
Mael PERREAULT <Maperreault@cstj.qc.ca>
2024-08-22 10:05:03 -0400

    Cours 02 - Ajout du projet

---
+++ Solution/Cours02_GitSyntaxe/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Cours02_GitSyntaxe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Mon nom est : Mael!");   
+        }
+    }
+}
