1ec19aa757dc24d1d375496785c32481d4e06929
Mael PERREAULT <Maperreault@cstj.qc.ca>
2024-08-22 10:46:17 -0400

    Cours 02 - Théorie

--- Solution/Cours02_GitSyntaxe/Program.cs
+++ Solution/Cours02_GitSyntaxe/Program.cs
@@ -5,7 +5,23 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Mon nom est : Mael!");   
+            Console.WriteLine("Mon nom est : Mael!");
+
+            int x = 0;
+
+            int resultatEntier = 10 + 20; // int : nombre entier
+            resultatEntier = resultatEntier + 100;
+            resultatEntier = resultatEntier * 10;
+            resultatEntier = resultatEntier / 3;
+
+            double resultatVirgule = 10.123 + 20.234; //double : nombre a virgule
+            char unCaractere = 'A';
+            string chaineDeCaractere = "Allo!";
+
+            Console.WriteLine(resultatEntier);
+            Console.WriteLine(resultatVirgule);
+            Console.WriteLine(unCaractere);
+            Console.WriteLine(chaineDeCaractere);
         }
     }
 }
