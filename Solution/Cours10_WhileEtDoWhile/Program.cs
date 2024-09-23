d038edb4341d824cd409f3de3b06372cd4664b57
Mael PERREAULT <Maperreault@cstj.qc.ca>
2024-09-23 09:24:48 -0400

    Cours 10 - Théorie

---
+++ Solution/Cours10_WhileEtDoWhile/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Cours10_WhileEtDoWhile
+{
+    internal class Program
+    {
+        const bool ACTIVER_THEORIE = true;
+
+        static void Main(string[] args)
+        {
+            if (ACTIVER_THEORIE)
+            {
+                string chaine = "";
+
+                string lettre = ""; // Initialiser
+                while (lettre != "-1") //Effectuée 0+ fois
+                {
+                    Console.Write("Entrer lettre : ");
+                    lettre = Console.ReadLine();
+
+                    if (lettre != "-1")
+                    {
+                        chaine += lettre;
+                        chaine += lettre;
+                    }
+
+                }
+
+                Console.WriteLine($"La chaine est : {chaine}");
+
+                chaine = "";
+                string lettreDoWhile; // Pas besoin initialiser 
+                do // Effectué 1+ fois
+                {
+                    Console.Write("Entrer lettre : ");
+                    lettreDoWhile = Console.ReadLine();
+
+                    if (lettreDoWhile != "-1")
+                    {
+                        chaine += lettreDoWhile;
+                        chaine += lettreDoWhile;
+                    }
+                } while (lettreDoWhile != "-1");
+
+                Console.WriteLine($"La chaine est : {chaine}");
+            }
+
+            // Exercices ...
+        }
+    }
+}
