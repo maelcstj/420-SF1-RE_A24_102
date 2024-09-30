a19f943121e8002095ce5e4c76e6ce8ea41ae6d6
Mael PERREAULT <Maperreault@cstj.qc.ca>
2024-09-30 10:38:58 -0400

    Cours 12 - Théorie

---
+++ Solution/Cours12_ExercicesBoucles/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Cours12_ExercicesBoucles
+{
+    internal class Program
+    {
+        const bool ACTIVER_THEORIE = true;
+
+        static void Main(string[] args)
+        {
+            if (ACTIVER_THEORIE)
+            {
+                // Demander a utilisateur un nombre tant que le nombre est pas valide
+                int nombre = 0;
+                bool estNombreValide = false;
+                while (estNombreValide == false)
+                {
+                    Console.Write("Entrer un nombre : ");
+                    string entreeNombre = Console.ReadLine();
+                    estNombreValide = int.TryParse(entreeNombre, out nombre);
+
+                    if (estNombreValide == false)
+                        Console.WriteLine("Erreur : nombre invalide !");
+                }
+
+                // Une fois le nombre valide, écrire Le nombre est : ###
+                Console.WriteLine($"Le nombre est : {nombre}");
+            }
+        }
+    }
+}
